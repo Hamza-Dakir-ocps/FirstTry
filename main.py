b5dd0b458afe187c15553280b91790626786b04a
Hamza DAKIR <hamza.dakir@ocpsolutions.ma>
2026-04-24 16:45:17 +0100

    Revert "deleting some extras"
    This reverts commit aeca92d70e9a00807fababbee67cd0a267cce1f9.

--- main.py
+++ main.py
@@ -4,5 +4,37 @@ from os import rename
 
 import requests
 
-print(sys.executable)
-print("requests work")
+
+def compute(a, b):
+    result = a + b
+    return result
+
+
+def fetch():
+    response = requests.get("https://example.com")
+    return response.status_code
+
+
+def stats(nums):
+    return Counter(nums)
+
+
+def format_time():
+    return datetime.now().strftime("%Y-%m-%d")
+
+
+numbers = [1, 2, 2, 3, 3, 3]
+print("Python:", sys.executable)
+
+value = compute(5, 10)
+print("Value:", value)
+
+status = fetch()
+print("Status:", status)
+
+print(stats(numbers))
+
+print(format_time())
+
+if True:
+    print("bad indent")
